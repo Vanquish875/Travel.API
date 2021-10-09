8809a416949d3786c6b57d29ae919458a5411d1c
Vanquish875 <Vanquish87523@gmail.com>
2021-10-08 21:58:51 -0500

    Add functionality to Activities and Boardings controllers.

---
+++ Travel.API/Controllers/ActivitiesController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Travel.API.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Travel.API.Model;
+
+namespace Travel.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActivitiesController : ControllerBase
+    {
+        private readonly TravelContext _context;
+
+        public ActivitiesController(TravelContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetAllActivities()
+        {
+            var activities = await _context.TravelActivities.ToListAsync();
+
+            if(activities == null)
+            {
+                return NotFound();
+            }
+
+            return activities;
+        }
+
+        [HttpGet("{cityId}")]
+        public async Task<ActionResult<List<Activity>>> GetActivitiesByCity(int cityId)
+        {
+            var activities = await _context.TravelActivities.Where(a => a.CityId == cityId).ToListAsync();
+
+            if(activities == null)
+            {
+                return NotFound();
+            }
+
+            return activities;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> CreateActivity([FromBody] Activity activity)
+        {
+            _context.Add(activity);
+            await _context.SaveChangesAsync();
+
+            return activity.ActivityId;
+        }
+    }
+}
